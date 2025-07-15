9076cfc90b5cdeb20097e570aac9ae27d4befc2e
Duck <duc240804@gmail.com>
2025-07-15 08:54:44 +0700

    Duc Commit

---
+++ frm_TrangChu.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace DA_Winform_QLQuanCaPhe
+{
+    partial class frm_TrangChu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.khoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_ban3 = new System.Windows.Forms.Button();
+            this.btn_ban8 = new System.Windows.Forms.Button();
+            this.btn_ban4 = new System.Windows.Forms.Button();
+            this.btn_ban9 = new System.Windows.Forms.Button();
+            this.btn_ban5 = new System.Windows.Forms.Button();
+            this.btn_ban6 = new System.Windows.Forms.Button();
+            this.btn_ban2 = new System.Windows.Forms.Button();
+            this.btn_ban7 = new System.Windows.Forms.Button();
+            this.btn_ban10 = new System.Windows.Forms.Button();
+            this.btn_ban1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.khoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1182, 28);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // khoToolStripMenuItem
+            // 
+            this.khoToolStripMenuItem.Name = "khoToolStripMenuItem";
+            this.khoToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.khoToolStripMenuItem.Text = "Kho";
+            // 
+            // btn_ban3
+            // 
+            this.btn_ban3.Location = new System.Drawing.Point(440, 178);
+            this.btn_ban3.Name = "btn_ban3";
+            this.btn_ban3.Size = new System.Drawing.Size(116, 181);
+            this.btn_ban3.TabIndex = 21;
+            this.btn_ban3.Text = "Bàn 3";
+            this.btn_ban3.UseVisualStyleBackColor = true;
+            // 
+            // btn_ban8
+            // 
+            this.btn_ban8.Location = new System.Drawing.Point(440, 392);
+            this.btn_ban8.Name = "btn_ban8";
+            this.btn_ban8.Size = new System.Drawing.Size(116, 181);
+            this.btn_ban8.TabIndex = 20;
+            this.btn_ban8.Text = "Bàn 8";
+            this.btn_ban8.UseVisualStyleBackColor = true;
+            // 
+            // btn_ban4
+            // 
+            this.btn_ban4.Location = new System.Drawing.Point(596, 178);
+            this.btn_ban4.Name = "btn_ban4";
+            this.btn_ban4.Size = new System.Drawing.Size(116, 181);
+            this.btn_ban4.TabIndex = 19;
+            this.btn_ban4.Text = "Bàn 4";
+            this.btn_ban4.UseVisualStyleBackColor = true;
+            // 
+            // btn_ban9
+            // 
+            this.btn_ban9.Location = new System.Drawing.Point(596, 392);
+            this.btn_ban9.Name = "btn_ban9";
+            this.btn_ban9.Size = new System.Drawing.Size(116, 181);
+            this.btn_ban9.TabIndex = 18;
+            this.btn_ban9.Text = "Bàn 9";
+            this.btn_ban9.UseVisualStyleBackColor = true;
+            // 
+            // btn_ban5
+            // 
+            this.btn_ban5.Location = new System.Drawing.Point(753, 178);
+            this.btn_ban5.Name = "btn_ban5";
+            this.btn_ban5.Size = new System.Drawing.Size(116, 181);
+            this.btn_ban5.TabIndex = 17;
+            this.btn_ban5.Text = "Bàn 5";
+            this.btn_ban5.UseVisualStyleBackColor = true;
+            // 
+            // btn_ban6
+            // 
+            this.btn_ban6.Location = new System.Drawing.Point(137, 392);
+            this.btn_ban6.Name = "btn_ban6";
+            this.btn_ban6.Size = new System.Drawing.Size(116, 181);
+            this.btn_ban6.TabIndex = 16;
+            this.btn_ban6.Text = "Bàn 6";
+            this.btn_ban6.UseVisualStyleBackColor = true;
+            // 
+            // btn_ban2
+            // 
+            this.btn_ban2.Location = new System.Drawing.Point(289, 178);
+            this.btn_ban2.Name = "btn_ban2";
+            this.btn_ban2.Size = new System.Drawing.Size(116, 181);
+            this.btn_ban2.TabIndex = 15;
+            this.btn_ban2.Text = "Bàn 2";
+            this.btn_ban2.UseVisualStyleBackColor = true;
+            // 
+            // btn_ban7
+            // 
+            this.btn_ban7.Location = new System.Drawing.Point(289, 392);
+            this.btn_ban7.Name = "btn_ban7";
+            this.btn_ban7.Size = new System.Drawing.Size(116, 181);
+            this.btn_ban7.TabIndex = 14;
+            this.btn_ban7.Text = "Bàn 7";
+            this.btn_ban7.UseVisualStyleBackColor = true;
+            // 
+            // btn_ban10
+            // 
+            this.btn_ban10.Location = new System.Drawing.Point(753, 392);
+            this.btn_ban10.Name = "btn_ban10";
+            this.btn_ban10.Size = new System.Drawing.Size(116, 181);
+            this.btn_ban10.TabIndex = 13;
+            this.btn_ban10.Text = "Mang về";
+            this.btn_ban10.UseVisualStyleBackColor = true;
+            // 
+            // btn_ban1
+            // 
+            this.btn_ban1.Location = new System.Drawing.Point(137, 178);
+            this.btn_ban1.Name = "btn_ban1";
+            this.btn_ban1.Size = new System.Drawing.Size(116, 181);
+            this.btn_ban1.TabIndex = 12;
+            this.btn_ban1.Text = "Bàn 1";
+            this.btn_ban1.UseVisualStyleBackColor = true;
+            // 
+            // frm_TrangChu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 660);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.btn_ban3);
+            this.Controls.Add(this.btn_ban8);
+            this.Controls.Add(this.btn_ban4);
+            this.Controls.Add(this.btn_ban9);
+            this.Controls.Add(this.btn_ban5);
+            this.Controls.Add(this.btn_ban6);
+            this.Controls.Add(this.btn_ban2);
+            this.Controls.Add(this.btn_ban7);
+            this.Controls.Add(this.btn_ban10);
+            this.Controls.Add(this.btn_ban1);
+            this.Name = "frm_TrangChu";
+            this.Text = "frm_TrangChu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem khoToolStripMenuItem;
+        private System.Windows.Forms.Button btn_ban3;
+        private System.Windows.Forms.Button btn_ban8;
+        private System.Windows.Forms.Button btn_ban4;
+        private System.Windows.Forms.Button btn_ban9;
+        private System.Windows.Forms.Button btn_ban5;
+        private System.Windows.Forms.Button btn_ban6;
+        private System.Windows.Forms.Button btn_ban2;
+        private System.Windows.Forms.Button btn_ban7;
+        private System.Windows.Forms.Button btn_ban10;
+        private System.Windows.Forms.Button btn_ban1;
+    }
+}
